b82c34e6e042facc1507a806a5139f483946ba97
Leontine Alkema <lalkema@umass.edu>
2026-03-06 14:08:23 -0500

    allow for obs up to 2026

--- R/fit_model.R
+++ R/fit_model.R
@@ -764,10 +764,10 @@ fit_model <- function(
   #   summarise(t_max = max(t)) %>%
   #   pull(t_max)
   # t_max <- ifelse(t_max < (t_stars+ 1), t_stars+ 1, t_max)
-  # update: setting t_max to 2024 to avoid issues when including EMUs
+  # update: setting t_max to 2026 to avoid issues when including routine data
   # this includes same dependence on start and end year as does t star
-  t_max <- rep(which(seq(start_year, end_year) == 2024), nrow(geo_unit_index))
-
+  if (end_year < 2026) stop("end_year less than 2026 not tested")
+  t_max <- rep(which(seq(start_year, end_year) == 2026), nrow(geo_unit_index))
 
   # put together stan data
   stan_data <- list(
